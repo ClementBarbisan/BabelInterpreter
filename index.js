83ac23ccaeec95ad1f17d6634a5dda76719dc83c
Clément Barbisan <c.barbisan@free.fr>
2025-01-22 23:04:32 +0100

    update babel interpreter

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-// import express JS module into app 
-// and creates its variable. 
-var express = require('express'); 
+var express = require('express');
 var app = express();
 var {PythonShell} = require('python-shell');
 var options;
+var log = false;
 
 app.use(express.static(__dirname + '/public'));
-// Creates a server which runs on port 3000 and 
-// can be accessed through localhost:3000 
-app.listen(3000, function() { 
+app.use(function (req, res, next) {
+	res.set('X-Clacks-Overhead', 'GNU Terry Pratchet');
+	next();
+});
+
+app.listen(3000, function() {
 	console.log('server running on port 3000'); 
 } )
 
@@ -17,8 +19,6 @@ app.get('/',function(req, res)
 	res.render(index.html);
 });
 
-// Function callName() is executed whenever 
-// url is of the form localhost:3000/name 
 app.get('/page', printPage);
 
 app.get('/address', printAddress);
@@ -28,14 +28,14 @@ function pythonBabel(err, results, res)
 	if (err)
 	{
 		console.log(err);
-		PythonShell.run('/home/BabelInterpreter/library_of_babel.py', options, function callback(err, results)
+		PythonShell.run(__dirname + '/library_of_babel.py', options, function callback(err, results)
 		{
 			pythonBabel(err, results, res)
 		});
 		return;
 	}
-	// results is an array consisting of messages collected during execution
-	console.log('results: %j', results);
+	if (log)
+		console.log('results: %j', results);
 	res.status(200).send(results[2]);
 }
 
@@ -46,7 +46,7 @@ function printAddress(req, res)
 		args: ['--checkout', req.query.search]
 	};
 
-	PythonShell.run('/home/BabelInterpreter/library_of_babel.py', options, function callback(err, results)
+	PythonShell.run(__dirname + '/library_of_babel.py', options, function callback(err, results)
 	{
 		pythonBabel(err, results, res)
 	});
@@ -57,12 +57,12 @@ function printPage(req, res) {
 	  mode: 'text',
 	  args: ['--search', req.query.search.substring(0, 3260)]
 	};
-	console.log(req.query.search);
-	PythonShell.run('/home/BabelInterpreter/library_of_babel.py', options, function callback(err, results)
+	if (log)
+		console.log(req.query.search);
+	PythonShell.run(__dirname + '/library_of_babel.py', options, function callback(err, results)
 	{
-		console.log(results);
+		if (log)
+			console.log(results);
 		pythonBabel(err, results, res)
 	});
-} 
-
-// save code as start.js 
+}
